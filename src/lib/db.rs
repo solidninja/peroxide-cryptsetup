6b46d931c9ffddbecb8042706b5a1ddfe2cfd00b
Vladimir Lushnikov <vladimir@solidninja.is>
2017-07-25 20:53:11 +0100

    Problem: No tests for serialization meant that serialization format changed silently when library was upgraded, causing breakage
    Solution: Add tests for serialization

--- src/lib/db.rs
+++ src/lib/db.rs
@@ -11,13 +11,13 @@ pub const DB_VERSION: u16 = 1;
 
 pub type Result<T> = io::Result<T>;
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq)]
 pub enum DbType {
 	Operation,
 	Backup,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct PeroxideDb {
 	pub entries: Vec<DbEntry>,
 	pub db_type: DbType,
@@ -96,7 +96,7 @@ impl PeroxideDb {
         where W: Write
     {
         let json = serde_json::to_string(self).unwrap();
-        try!(writer.write_all(json.as_bytes()));
+        writer.write_all(json.as_bytes())?;
         Ok(())
     }
 }
@@ -115,4 +115,77 @@ impl DbEntry {
     }
 }
 
-// TODO: Write tests for the serialization
+#[cfg(test)]
+pub mod tests {
+
+    use super::*;
+
+    use expectest::prelude::*;
+
+    use std::path::PathBuf;
+    use serde_json;
+    use uuid::Uuid;
+
+    #[test]
+    fn test_serialize_db_type() {
+        expect!(serde_json::to_string(&DbType::Operation)).to(be_ok().value(r#""Operation""#.to_string()));
+        expect!(serde_json::to_string(&DbType::Backup)).to(be_ok().value(r#""Backup""#.to_string()));
+    }
+
+    #[test]
+    fn test_serialize_yubikey_entry_type() {
+        expect!(serde_json::to_string(&YubikeyEntryType::ChallengeResponse)).to(be_ok().value(r#""ChallengeResponse""#.to_string()));
+        expect!(serde_json::to_string(&YubikeyEntryType::HybridChallengeResponse)).to(be_ok().value(r#""HybridChallengeResponse""#.to_string()));
+    }
+
+    #[test]
+    fn test_serialize_volume_id() {
+        expect!(serde_json::to_string(&VolumeId::new(None, Uuid::nil()))).to(be_ok().value(r#"{"name":null,"id":{"uuid":"00000000-0000-0000-0000-000000000000"}}"#.to_string()));
+        expect!(serde_json::to_string(&VolumeId::new(Some("foobar".to_string()), Uuid::nil()))).to(be_ok().value(r#"{"name":"foobar","id":{"uuid":"00000000-0000-0000-0000-000000000000"}}"#.to_string()));
+    }
+
+    #[test]
+    fn test_serialize_keyfile_entry() {
+        let entry = DbEntry::KeyfileEntry {
+            key_file: PathBuf::from("/path/to/keyfile"),
+            volume_id: VolumeId::new(None, Uuid::nil())
+        };
+        expect!(serde_json::to_string(&entry)).to(be_ok().value(r#"{"KeyfileEntry":{"key_file":"/path/to/keyfile","volume_id":{"name":null,"id":{"uuid":"00000000-0000-0000-0000-000000000000"}}}}"#.to_string()));
+    }
+
+    #[test]
+    fn test_serialize_passphrase_entry() {
+        let entry = DbEntry::PassphraseEntry {
+            volume_id: VolumeId::new(None, Uuid::nil())
+        };
+        expect!(serde_json::to_string(&entry)).to(be_ok().value(r#"{"PassphraseEntry":{"volume_id":{"name":null,"id":{"uuid":"00000000-0000-0000-0000-000000000000"}}}}"#.to_string()));
+    }
+
+    #[test]
+    fn test_serialize_yubikey_entry() {
+        let entry = DbEntry::YubikeyEntry {
+            entry_type: YubikeyEntryType::HybridChallengeResponse,
+            slot: 1,
+            volume_id: VolumeId::new(None, Uuid::nil())
+        };
+        expect!(serde_json::to_string(&entry)).to(be_ok().value(r#"{"YubikeyEntry":{"entry_type":"HybridChallengeResponse","slot":1,"volume_id":{"name":null,"id":{"uuid":"00000000-0000-0000-0000-000000000000"}}}}"#.to_string()));
+    }
+
+    #[test]
+    fn test_serialize_database() {
+        let db = PeroxideDb::new(DbType::Operation);
+        expect!(serde_json::to_string(&db)).to(be_ok().value(r#"{"entries":[],"db_type":"Operation","version":1}"#));
+    }
+
+    #[test]
+    fn test_deserialize_small_database() {
+        let db_json = r#"{"entries":[{"KeyfileEntry":{"key_file":"keyfile.key","volume_id":{"name":"test-disk","id":{"uuid":"00000000-0000-0000-0000-000000000000"}}}}],"db_type":"Backup","version":1}"#;
+        let mut db = PeroxideDb::new(DbType::Backup);
+        db.entries.push(DbEntry::KeyfileEntry {
+            key_file: PathBuf::from("keyfile.key"),
+            volume_id: VolumeId::new(Some("test-disk".to_string()), Uuid::nil())
+        });
+        expect!(serde_json::from_str::<PeroxideDb>(db_json)).to(be_ok().value(db.clone()));
+    }
+
+}
